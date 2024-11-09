b3ef9e3a7df9324d2d3e096cbdfccd901b95d630
Faris <farisruknuddin@gmail.com>
2024-11-09 15:47:26 +0530

    commited successfully

---
+++ script.js
@@ -0,0 +1,8 @@
+function showAlert() {
+    alert("More details coming soon!");
+}
+
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for reaching out! I will get back to you soon.');
+});
